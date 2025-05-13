fe4b2e754b6357aab3541a193b6e69be8e369d5c
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-13 21:09:55 +0900

    chore: update config

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,23 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://mimikun.dev/",
+  author: "mimikun",
+  profile: "",
+  desc: "mimikun's blog",
+  title: "mimikun.dev",
+  // TODO: it
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 15 * 60 * 1000, // 15 min
   showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: true,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/mimikun/mimikun.dev/edit/master",
   },
   dynamicOgImage: true,
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "ja",
+  timezone: "Asia/Tokyo",
 } as const;
